d51d7786cb8a9db0c6781d361ec08098a8047950
Blaire <grant.blaire@gmail.com>
2021-05-27 07:24:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        name,
+        email
+    }
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    // const tag = document.createElement('h3');
+    document.body.innerHTML = object["id"];
+    // tag.innerText = `${object.id}`;
+    // console.log(object);
+  })
+  .catch(function(error) {
+    alert("Unauthorized Access");
+    // const errorMessage = document.createElement("h3");
+    document.body.innerHTML = error.message;
+    // errorMessage.innerText = `${error.message}`;
+  }); 
+};
